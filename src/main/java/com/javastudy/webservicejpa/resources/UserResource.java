94b93cc6cf33a52fd21f35779df3bab46f165fa0
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-08 12:16:36 -0300

    creating user entity and controller

---
+++ src/main/java/com/javastudy/webservicejpa/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.javastudy.webservicejpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javastudy.webservicejpa.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "999999", "123456");
+		return ResponseEntity.ok().body(user);
+	}
+}
