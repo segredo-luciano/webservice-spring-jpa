1c20eb117ae8986f6bf31145092b2c2bbbf1ef28
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-08 11:24:40 -0300

    creating project

---
+++ src/main/java/com/javastudy/webservicejpa/WebserviceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.javastudy.webservicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javastudy/webservicejpa/WebserviceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastudy.webservicejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
