0fc184cfc9f970e1e8e9a8f259fcdc09626c9878
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-09 12:11:24 -0300

    creating service layer and endpoints

--- src/main/java/com/javastudy/webservicejpa/resources/UserResource.java
+++ src/main/java/com/javastudy/webservicejpa/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.javastudy.webservicejpa.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javastudy.webservicejpa.entities.User;
+import com.javastudy.webservicejpa.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Maria", "maria@gmail.com", "999999", "123456");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = userService.findAll();
+		return ResponseEntity.ok().body(userList);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
