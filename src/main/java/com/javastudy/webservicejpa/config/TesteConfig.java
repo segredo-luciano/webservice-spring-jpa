be93557f9f84417fae7132eccf42970ed6191c07
segredo-luciano <lucianosegredo20@gmail.com>
2023-10-13 11:06:02 -0300

    payment association one to one

--- src/main/java/com/javastudy/webservicejpa/config/TesteConfig.java
+++ src/main/java/com/javastudy/webservicejpa/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.javastudy.webservicejpa.entities.Category;
 import com.javastudy.webservicejpa.entities.Order;
 import com.javastudy.webservicejpa.entities.OrderItem;
+import com.javastudy.webservicejpa.entities.Payment;
 import com.javastudy.webservicejpa.entities.Product;
 import com.javastudy.webservicejpa.entities.User;
 import com.javastudy.webservicejpa.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
